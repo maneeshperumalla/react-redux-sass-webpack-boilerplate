9f0d958c5d7da647a8fc6bb4394e5d09e714c79d
Maneesh Perumalla <maneeshperumalla@gmail.com>
2020-10-14 00:09:20 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: "",
+  },
+  mode: "development",
+  devtool: "source-map",
+  watch: true,
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
